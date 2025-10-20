379b2fdfefd08c2e0ef0ec672c69fa0bd839f1e1
matheuskdr <matheusgabriellkpo@gmail.com>
2025-10-20 16:12:14 -0300

    creating home header

--- src/app/(ui)/home/page.tsx
+++ src/app/(ui)/home/page.tsx
@@ -1,3 +1,9 @@
+import { HomeHeader } from "@/components/home/home-header";
+
 export default function Page() {
-    return <div className="">...</div>;
+    return (
+        <div className="">
+            <HomeHeader />
+        </div>
+    );
 }
